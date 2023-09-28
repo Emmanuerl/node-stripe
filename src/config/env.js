0216636d0b326e6d665b4f5b7ffe275bba4306b1
Chukwuemeka chukwurah <emekaemmanuel045@gmail.com>
2023-09-28 11:50:41 +0100

    chore: setup joi validation (#1)

---
+++ src/config/env.js
@@ -0,0 +1,32 @@
+const joi = require("joi");
+const { validate, DataValidationError } = require("../internals/joi");
+
+require("dotenv").config();
+
+const schema = {
+  port: joi.number().required(),
+  stripe_secret_key: joi.string().required(),
+  database_url: joi.string().uri()
+}
+
+/**
+ * Loads the required environment variables into a map for easy access across the app.
+ * @returns the map of env values wtth key converted to it's lower case form
+ */
+function loadEnv() {
+  const lowerisedEnv = Object.keys(process.env).reduce((obj, key) => {
+    obj[key.toLowerCase()] = process.env[key];
+    return obj;
+  }, {});
+
+  try {
+    return validate(lowerisedEnv, schema);
+  } catch (err) {
+    if (err instanceof DataValidationError) {
+      throw new Error(JSON.stringify(err.messages, null, 2))
+    }
+    throw err;
+  }
+}
+
+module.exports = loadEnv();
